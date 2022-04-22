f81ede45a59be3050405b7a14aea0487e0416b9a
Sergey Babich <88515146+KreoManser@users.noreply.github.com>
2022-04-22 22:12:28 +0300

    Test commit to merge files

--- src/data_structure.cpp
+++ src/data_structure.cpp
@@ -1,9 +1,12 @@
-#include "data_structure.hpp"
+#include "segment_tree.hpp"
 
 // файл с определениями
 
 namespace itis {
 
   // здесь должны быть определения методов вашей структуры
+  SegmentTree::SegmentTree(int size, int *array) {
+
+  }
 
 }  // namespace itis
