fbb51b6d226f35ee5de0c7b7e683daade78090aa
Sergey <lyamkin.serezha@mail.ru>
2022-04-23 02:08:59 +0700

    test merge commit

--- src/segment_tree.cpp
+++ src/segment_tree.cpp
@@ -1,9 +1,12 @@
-#include "data_structure.hpp"
+#include "segment_tree.hpp"
+#include <iostream>
 
 // файл с определениями
 
 namespace itis {
 
   // здесь должны быть определения методов вашей структуры
+
+  ~SegmentTree();
 
 }  // namespace itis
